e902cf64a4994986392dd3f30be36f7c667e4f1c
Usta2301 <aev.usta23@gmail.com>
2025-02-27 07:47:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,39 @@ st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Pasta2.png')
 
 st.image(image, caption='Pasta2')
+
+
+text = st.text_input('Escribe algo','Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora useamos 2 Columnas")
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto!')
+    
+with col2:
+  modo = st.radio("Que Modadlidad es la principal en tu interfaz", ('Visual','auditiva', 'Tactil'))    
+  if modo == 'Visual'
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'auditiva':
+    st.write('La audicion es fundamental para tu interfaz')
+  if modo == 'Tactil':
+    st.write('El tacto es fundamental para tu interfaz')
+
+st.subheader("Uso de Botones")
+if st.button('Presiona el botón'):
+    st.write('Graciaspor presionar')
+else
+  st.write('No has presionado aún')
+
+st.subheader("Selectbox")
+in_mod = st.selectbox(
+  "Selecciona la modalidad",
+  ("Audio", "Visual", "Haptico"),
+)
+if in_mod == "Audio":
+  
