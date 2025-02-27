a22fa2cf320b09386f4a167bd76ed38a4234502e
Usta2301 <aev.usta23@gmail.com>
2025-02-27 08:54:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.title("Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('Pasta2.png')
+image = Image.open('Pasta.jpg')
 
-st.image(image, caption='Pasta2')
+st.image(image, caption='Pasta')
 
 
 texto = st.text_input('Escribe algo','Este es mi texto')
