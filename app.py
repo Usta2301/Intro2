49369a53314619a3436e8eec96039561627ac02f
Usta2301 <aev.usta23@gmail.com>
2025-02-27 07:24:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Mi Primera App!!")
 
-
-
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Pasta2.png')
 
 st.image(image, caption='Pasta2')
